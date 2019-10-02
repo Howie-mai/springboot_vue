441137d66c8a73bd449f0844d4865c49f9af98f4
Howie.Mai <494210688@qq,com>
2019-10-03 03:24:23 +0800

    add nginx反向代理 共享session

---
+++ springboot_vue/server_session_share_demo/src/main/java/com/zhku/mh/ServerSessionShareDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zhku.mh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSessionShareDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerSessionShareDemoApplication.class, args);
+    }
+
+}
